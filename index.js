09276eed274d4336588011401878e6d94aa525d8
Ryan Megidov <Ryan Megidov>
2016-07-21 19:56:17 +0200

    Fix dependency mess

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 'use strict';
 
 var mobxConnect;
+var mobxInferno;
+var mobxReact;
 
 try {
-    var mobxReact = require('mobx-react');
-    mobxConnect = require('./src/connect')(mobxReact);
-} catch(e) {
-    var mobxInferno = require('mobx-inferno');
+    mobxInferno = require('mobx-inferno');
     mobxConnect = require('./src/connect')(mobxInferno);
-}
+} catch(e) {}
+
+try {
+    mobxReact = require('mobx-react');
+    mobxConnect = require('./src/connect')(mobxReact);
+} catch(e) {}
+
+if (mobxInferno && mobxReact)
+    throw new Error('It looks like you are using both Inferno and React. This can lead to unexpected behavior.\n' +
+    'Perhaps you can fix this by aliasing in your webpack/browserify configuration.')
 
 module.exports = mobxConnect;
