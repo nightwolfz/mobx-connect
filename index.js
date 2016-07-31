f1b0798f04c233b8bda639754f09d67239e0659a
Ryan Megidov <Ryan Megidov>
2016-07-31 16:37:31 +0200

    ES5/ES6 export compatibility

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
 'use strict';
 
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var mobxConnect;
 var mobxInferno;
 var mobxReact;
 
 try {
     mobxInferno = require('mobx-inferno');
+    exports.default = require('./src/connect')(mobxInferno);
 } catch(e) {}
 
 try {
     mobxReact = require('mobx-react');
-} catch(e) {}
+    exports.default = require('./src/connect')(mobxReact);
 
-
-if (mobxInferno && mobxReact) {
-    throw new Error('It looks like you are using both Inferno and React. This can lead to unexpected behavior.\n' +
-    'Perhaps you can fix this by aliasing in your webpack/browserify configuration.')
-}
-
-if (!mobxInferno && !mobxReact) {
-    throw new Error('You need to have either `mobx-react` or `mobx-inferno` installed.\n' +
+} catch(e) {
+    console.error('WARNING:',
+    'You need to have either `mobx-react` or `mobx-inferno` installed.\n',
     'Example: npm install mobx-react --save')
 }
 
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = require('./src/connect')(mobxInferno || mobxReact);
+if (mobxInferno && mobxReact) {
+    console.error('WARNING:',
+    'It looks like you are using both Inferno and React. This can lead to unexpected behavior.\n',
+    'Perhaps you can fix this by aliasing in your webpack/browserify configuration.')
+}
+
 module.exports = exports['default'];
